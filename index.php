5a04ff0b50d797a19672ace1a66baf7aba088ab5
ValsG971 <53057650+ValsG971@users.noreply.github.com>
2020-01-27 13:48:24 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/* Ce programme affiche Bonjour */
+    echo 'Bonjour';
+
+/* déclarer les variables de travail */
+
+$a = 1;
+$b = 5; 
+
+// calculer la somme 
+ $s = $a + $b;
+
+ echo '<p>La somme de '.$a.' et '.$b.' est : '.$s.'</p>';
+
+ echo "<p>La somme de $a et $b est $s</p>"; // cette forme est plus lente à exécuter par le compilateur
+?>
---
+++ sommeView.php
@@ -0,0 +1,22 @@
+<?php
+    $a = 1;
+    $b = 5; 
+    
+    // calculer la somme 
+     $s = $a + $b;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <style>
+        #somme {
+            background: green;
+            color: yellow;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <div id="somme" >La somme de <?=$a;?> et <?=$b;?> est égale à <?=$s;?></div>
+</body>
+</html>
